7132c526eaca9c5f392fe36f04dc60becb6ebb91
piojeet <manjhipiojeet@gmail.com>
2023-09-06 13:44:07 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,481 @@
+
+// ------------------- quntar button ----------------
+let a = 1;
+
+function plus1() {
+  if(a < 10){
+    a++;
+  document.getElementById("num1").innerHTML=a;
+  }
+  
+}
+
+function minus1() {
+  if(a > 1){
+    a--;
+  document.getElementById("num1").innerHTML=a;
+  }
+  
+}
+
+
+
+
+let b = 1;
+
+function plus2() {
+  if(b < 10){
+    b++;
+  document.getElementById("num2").innerHTML=b;
+  }
+  
+}
+
+function minus2() {
+  if(b > 1){
+    b--;
+  document.getElementById("num2").innerHTML=b;
+  }
+  
+}
+
+
+
+
+let c = 1;
+
+function plus3() {
+  if(c < 10){
+    c++;
+  document.getElementById("num3").innerHTML=c;
+  }
+  
+}
+
+function minus3() {
+  if(c > 1){
+    c--;
+  document.getElementById("num3").innerHTML=c;
+  }
+  
+}
+
+
+
+
+
+
+
+let d = 1;
+
+function plus4() {
+  if(d < 10){
+    d++;
+  document.getElementById("num4").innerHTML=d;
+  }
+  
+}
+
+function minus4() {
+  if(d > 1){
+    d--;
+  document.getElementById("num4").innerHTML=d;
+  }
+  
+}
+
+
+
+
+
+
+let e = 1;
+
+function plus5() {
+  if(e < 10){
+    e++;
+  document.getElementById("num5").innerHTML=e;
+  }
+  
+}
+
+function minus5() {
+  if(e > 1){
+    e--;
+  document.getElementById("num5").innerHTML=e;
+  }
+  
+}
+
+
+
+
+
+
+
+let f = 1;
+
+function plus6() {
+  if(f < 10){
+    f++;
+  document.getElementById("num6").innerHTML=f;
+  }
+  
+}
+
+function minus6() {
+  if(f > 1){
+    f--;
+  document.getElementById("num6").innerHTML=f;
+  }
+  
+}
+
+
+
+
+
+
+let g = 1;
+
+function plus7() {
+  if(g < 10){
+    g++;
+  document.getElementById("num7").innerHTML=g;
+  }
+  
+}
+
+function minus7() {
+  if(g > 1){
+    g--;
+  document.getElementById("num7").innerHTML=7;
+  }
+  
+}
+
+
+
+
+
+let h = 1;
+
+function plus8() {
+  if(h < 10){
+    h++;
+  document.getElementById("num8").innerHTML=h;
+  }
+  
+}
+
+function minus8() {
+  if(h > 1){
+    h--;
+  document.getElementById("num8").innerHTML=h;
+  }
+  
+}
+
+
+
+let i = 1;
+
+function plus9() {
+  if(i < 10){
+    i++;
+  document.getElementById("num9").innerHTML=i;
+  }
+  
+}
+
+function minus9() {
+  if(i > 1){
+    i--;
+  document.getElementById("num9").innerHTML=i;
+  }
+  
+}
+
+
+
+let j = 1;
+
+function plus10() {
+  if(j < 10){
+    j++;
+  document.getElementById("num10").innerHTML=j;
+  }
+  
+}
+
+function minus10() {
+  if(j > 1){
+    j--;
+  document.getElementById("num10").innerHTML=j;
+  }
+  
+}
+
+
+
+let k = 1;
+
+function plus11() {
+  if(k < 10){
+    k++;
+  document.getElementById("num11").innerHTML=k;
+  }
+  
+}
+
+function minus11() {
+  if(k > 1){
+    k--;
+  document.getElementById("num11").innerHTML=k;
+  }
+  
+}
+
+
+let l = 1;
+
+function plus12() {
+  if(l < 10){
+    l++;
+  document.getElementById("num12").innerHTML=l;
+  }
+  
+}
+
+function minus12() {
+  if(l > 1){
+    l--;
+  document.getElementById("num12").innerHTML=l;
+  }
+  
+}
+
+
+
+
+let m = 1;
+
+function plus13() {
+  if(m < 10){
+    m++;
+  document.getElementById("num13").innerHTML=m;
+  }
+  
+}
+
+function minus13() {
+  if(m > 1){
+    m--;
+  document.getElementById("num13").innerHTML=m;
+  }
+  
+}
+
+
+
+let n = 1;
+
+function plus14() {
+  if(n < 10){
+    n++;
+  document.getElementById("num14").innerHTML=n;
+  }
+  
+}
+
+function minus14() {
+  if(n > 1){
+    n--;
+  document.getElementById("num14").innerHTML=n;
+  }
+  
+}
+
+
+
+
+let o = 1;
+
+function plus15() {
+  if(o < 10){
+    o++;
+  document.getElementById("num15").innerHTML=o;
+  }
+  
+}
+
+function minus15() {
+  if(o > 1){
+    o--;
+  document.getElementById("num15").innerHTML=o;
+  }
+  
+}
+
+// ------------------- quntar button ----------------
+
+
+
+
+
+
+
+
+// ------------------- heart button ----------------
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+
+  function myFunction2() {
+    var x = document.getElementById("myDIV2");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+
+
+  function myFunction3() {
+    var x = document.getElementById("myDIV3");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+
+  function myFunction4() {
+    var x = document.getElementById("myDIV4");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+
+  function myFunction5() {
+    var x = document.getElementById("myDIV5");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+
+  function myFunction6() {
+    var x = document.getElementById("myDIV6");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+
+
+  function myFunction7() {
+    var x = document.getElementById("myDIV7");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+
+  function myFunction8() {
+    var x = document.getElementById("myDIV8");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+
+
+  function myFunction9() {
+    var x = document.getElementById("myDIV9");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+
+  function myFunction10() {
+    var x = document.getElementById("myDIV10");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction11() {
+    var x = document.getElementById("myDIV11");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+
+  function myFunction12() {
+    var x = document.getElementById("myDIV12");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction13() {
+    var x = document.getElementById("myDIV13");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+
+
+  function myFunction14() {
+    var x = document.getElementById("myDIV14");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+
+
+  function myFunction15() {
+    var x = document.getElementById("myDIV15");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+// ------------------- heart button ----------------
